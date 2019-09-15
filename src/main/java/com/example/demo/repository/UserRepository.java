074d978258b0b180176a0a8beaa279a56e934feb
shenqiangbin <shenqiangbin@163.com>
2019-09-15 16:49:50 +0800

    结构调整 - 添加 Repository 层

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.example.demo.repository;
+
+import com.example.demo.base.SQLHelper;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private SQLHelper sqlHelper;
+
+    public User getUserByUserCode(String userCode) throws Exception {
+
+        String sql = "select * from user where usercode = ? and status = 1";
+        List<Object> params = new ArrayList<>();
+        params.add(userCode);
+
+        List<User> users = sqlHelper.query(sql, params, User.class);
+        if(users!=null && users.size() > 0)
+            return users.get(0);
+        else if(users.size() > 1)
+            throw new Exception("usercode重复:" + userCode);
+
+        return null;
+    }
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,33 +1,20 @@
 package com.example.demo.service;
 
-import com.example.demo.base.SQLHelper;
 import com.example.demo.model.User;
 
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private SQLHelper sqlHelper;
-	
-	public User getUserByUserCode(String userCode) throws Exception {
-		
-		String sql = "select * from user where usercode = ? and status = 1";
-		List<Object> params = new ArrayList<>();
-		params.add(userCode);
-		
-		List<User> users = sqlHelper.query(sql, params, User.class);
-		if(users!=null && users.size() > 0)
-			return users.get(0);
-		else if(users.size() > 1)
-			throw new Exception("usercode重复:" + userCode);
-		
-		return null;
+	private UserRepository userRepository;
+
+	public User getUserByUserCode(String userCode) throws Exception{
+		return userRepository.getUserByUserCode(userCode);
 	}
+
 }
