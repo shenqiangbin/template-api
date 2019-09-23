eb2449a31b91858151db51ae5d3f06f7f683977f
shenqiangbin <shenqiangbin@163.com>
2019-09-23 20:55:11 +0800

    修改密码 - 修改后需要重新登录

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,10 +6,12 @@ import com.example.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
@@ -21,7 +23,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/user/changePwd")
-    public Response changePwd(String oldPwd, String newPwd, Principal user) {
+    public Response changePwd(String oldPwd, String newPwd, Principal user, HttpSession session) {
 
         Response r = new Response();
 
@@ -34,6 +36,11 @@ public class UserController {
             if (usr.getPassword().equals(oldPwd)) {
                 boolean success = userService.ChangePwd(loginUser, newPwd);
                 if (success) {
+
+                    //令 Session 失效，表示退出登录
+                    session.invalidate();
+                    //SecurityContextHolder.clearContext();
+
                     r.set(200, "密码更新成功");
                 } else {
                     r.set(501, "密码更新失败");
