d8ab588b767480cd921b2ec7f99c298fb536fd8c
shenqiangbin <1348907384@qq.com>
2021-03-02 12:44:48 +0800

    手机登录验证

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,29 @@
 package com.example.demo;
 
 import com.example.demo.security.CustomUserDetailService;
+import com.example.demo.security.MyAuthenticationFailureHandler;
+import com.example.demo.security.MyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.session.MapSession;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +31,7 @@ import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 @RestController
@@ -42,23 +54,41 @@ public class DemoApplication {
 	    return user;
     }
 
+    @EnableSpringHttpSession
+    @EnableWebSecurity
     @Configuration
     //@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
     protected  static class  SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		private CustomUserDetailService customUserDetailService;
+        @Bean
+        public SessionRepository<MapSession> sessionRepository() {
+            return new MapSessionRepository(new ConcurrentHashMap<>());
+        }
+
+        @Bean
+        public HttpSessionIdResolver sessionIdResolver() {
+            return HeaderHttpSessionIdResolver.xAuthToken();
+        }
+
+        @Autowired
+        private CustomUserDetailService customUserDetailService;
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             //super.configure(http);
             http
-                    .httpBasic()
-                    .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .and()
+                    //.httpBasic()
+                    //.and().
+                    .formLogin()
+                    .successHandler(new MyAuthenticationSuccessHandler())
+                    .failureHandler(new MyAuthenticationFailureHandler())
+                    //.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .and().csrf().disable()
                     .authorizeRequests()
-                    .antMatchers("/index.html", "/", "home", "login").permitAll()
+                    .antMatchers("/index.html", "/", "home", "login", "/tel/login").permitAll()
                     .anyRequest().authenticated();
+
+
         }
 
         @Override
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,13 +6,20 @@ import com.example.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -21,6 +28,21 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/user/info")
+    private String info() {
+        return "info";
+    }
+
+    @GetMapping("/tel/login")
+    public Response<String> telLogin(String tel, String smsCode, HttpSession session) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("user"));
+        Authentication auth = new UsernamePasswordAuthenticationToken("tel", "", authorities);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String sessionId = session.getId();
+        return new Response(200, sessionId);
+    }
 
     @PostMapping("/user/changePwd")
     public Response changePwd(String oldPwd, String newPwd, Principal user, HttpSession session) {
